26011d1ea60b55786b42cc06614a7c136bff1683
Pema Sherpa <pematamang111@gmail.com>
2024-04-03 18:15:11 -0400

    add app use routes

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const htmlRoute = require('./htmlRoute.js');
 const notesRoute = require('./notesRoute.js');
 
+router.use(htmlRoute);
+router.use(notesRoute);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //dependencies
 const express = require('express');
 const path = require('path');
+const routes = require('./routes/index.js');
 //app and port
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -12,19 +13,8 @@ app.use(express.urlencoded ({ extended: true }));
 
 // Middleware to serve up static assets from the public folder
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-})
+//middleware for routes
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
